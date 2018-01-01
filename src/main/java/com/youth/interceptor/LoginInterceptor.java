eebda55d677ae4f33a381788966ad256971175da
xy3211285 <xiongyin1990@163.com>
2018-01-01 15:43:50 +0800

    添加拦截器
    1.添加拦截器
    2.添加home主页

---
+++ src/main/java/com/youth/interceptor/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.youth.interceptor;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!StringUtils.equals(request.getServletPath(), "/home")) {
+            response.sendRedirect("/home");
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/youth/interceptor/WebAppConfigurer.java
@@ -0,0 +1,13 @@
+package com.youth.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebAppConfigurer extends WebMvcConfigurerAdapter {
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+}
